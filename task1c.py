a6e65144f2e5c627b4b192cdbb7a983ec4bd50f2
Reni Dada <R.Dada@albynschool.org>
2026-03-20 09:21:15 +0000

    hi

---
+++ task1c.py
@@ -0,0 +1,15 @@
+#  First program
+#  By Reni
+#  10/03/2026
+# Task 1a
+
+# Task 1c
+print("What is your name?") 
+name = input()
+print("What is your age?") 
+age = input()
+print("What is your favourite colour?") 
+colour = input()
+print("Your name is", name)
+print(name, "is", age, "years old")
+print( name,"'s favourite colour is" ,colour)
